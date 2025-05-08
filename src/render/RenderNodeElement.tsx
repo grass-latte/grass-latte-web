da13737e11e95c3999b8f51772e0aa9428686dcf
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-05-08 17:46:45 +0100

    Update

--- src/render/RenderNodeElement.tsx
+++ src/render/RenderNodeElement.tsx
@@ -1,26 +1,40 @@
-import {type ReactNode} from "react";
+import {type ReactNode, useState} from "react";
 import AnyElement from "../AnyElement.tsx";
 import type {NodeElement} from "../backend/log_tree.ts";
+import {CaretDown, CaretRight} from "react-bootstrap-icons";
+import {AnimatePresence, motion} from "framer-motion";
 
 interface Props {
     node: NodeElement;
 }
 
 export function RenderNodeElement({node}: Props): ReactNode {
-    return <div key={node.id.toString()} className="w-auto mb-2">
-        <h3 className="mb-0">{node.id}</h3>
-        <div className="d-flex flex-row">
-            <div style={{width: "30px", height: "auto"}}>
-                <div className="d-flex flex-row h-100 w-100">
-                    <div className={"h-100"} style={{width: "4px"}}></div>
-                    <div className={"h-100"} style={{width: "1px", borderLeft: "1px dashed gray"}}></div>
-                </div>
-            </div>
+    const [isOpen, setIsOpen] = useState(true);
+    const toggle = () => setIsOpen((prev) => !prev);
 
-            <div className="flex-grow-1">
-                {node.children.size > 0 && <div className="mb-2"></div>}
-                {[...node.children].map(([i, v]) => <AnyElement key={i} element={v}/>)}
-            </div>
-        </div>
+
+    return <div key={node.id.toString()} className="w-auto mb-2">
+        <h3 className="mb-0" onClick={toggle}>{isOpen ? <CaretDown/> : <CaretRight/>} {node.id}</h3>
+        <AnimatePresence initial={false}>
+            {isOpen && <motion.div className="d-flex flex-row"
+                                   key="content"
+                                   initial={{ height: 0 }}
+                                   animate={{ height: "auto" }}
+                                   exit={{ height: 0 }}
+                                   transition={{ duration: 0.1, ease: "easeInOut" }}
+                                   style={{ overflow: "hidden" }}>
+                <div style={{width: "30px", height: "auto"}}>
+                    <div className="d-flex flex-row h-100 w-100">
+                        <div className={"h-100"} style={{width: "4px"}}></div>
+                        <div className={"h-100"} style={{width: "1px", borderLeft: "1px dashed gray"}}></div>
+                    </div>
+                </div>
+
+                <div className="flex-grow-1">
+                    {node.children.size > 0 && <div className="mb-2"></div>}
+                    {[...node.children].map(([i, v]) => <AnyElement key={i} element={v}/>)}
+                </div>
+            </motion.div>}
+        </AnimatePresence>
     </div>;
 }
